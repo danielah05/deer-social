d56d5ac4fea4a292586ac2e3d3057d479534fcc9
Aviva Ruben <aviva@rubenfamily.com>
2025-04-23 13:45:36 -0500

    pick the v0 constellations client

---
+++ src/state/queries/constellation.ts
@@ -0,0 +1,127 @@
+type ConstellationLink = {
+  did: string
+  collection: string
+  rkey: string
+}
+
+type Collection =
+  | 'app.bsky.actor.profile'
+  | 'app.bsky.feed.generator'
+  | 'app.bsky.feed.like'
+  | 'app.bsky.feed.post'
+  | 'app.bsky.feed.repost'
+  | 'app.bsky.feed.threadgate'
+  | 'app.bsky.graph.block'
+  | 'app.bsky.graph.follow'
+  | 'app.bsky.graph.list'
+  | 'app.bsky.graph.listblock'
+  | 'app.bsky.graph.listitem'
+  | 'app.bsky.graph.starterpack'
+  | 'app.bsky.graph.verification'
+  | 'chat.bsky.actor.declaration'
+
+const headers = new Headers({
+  Accept: 'application/json',
+  'User-Agent': 'deer.social (contact @aviva.gay)',
+})
+
+const makeReqUrl = (
+  instance: string,
+  route: string,
+  params: Record<string, string>,
+) => {
+  const url = new URL(instance)
+  url.pathname = route
+  for (const [k, v] of Object.entries(params)) {
+    url.searchParams.set(k, v)
+  }
+  return url
+}
+
+// using an async generator lets us kick off dependent requests before finishing pagination
+// this doesn't solve the gross N+1 queries thing going on here to get records, but it should make it faster :3
+export async function* constellationLinks(
+  instance: string,
+  params: {
+    target: string
+    collection: Collection
+    path: string
+  },
+) {
+  const url = makeReqUrl(instance, 'links', params)
+
+  const req = async () =>
+    (await (await fetch(url, {method: 'GET', headers})).json()) as {
+      total: number
+      linking_records: ConstellationLink[]
+      cursor: string | null
+    }
+
+  let cursor: string | null = null
+  while (true) {
+    const resp = await req()
+
+    for (const link of resp.linking_records) {
+      yield link
+    }
+
+    cursor = resp.cursor
+    if (cursor === null) break
+    url.searchParams.set('cursor', cursor)
+  }
+}
+
+export async function constellationCounts(
+  instance: string,
+  params: {target: string},
+) {
+  const url = makeReqUrl(instance, 'links/all', params)
+  const json = (await (await fetch(url, {method: 'GET', headers})).json()) as {
+    links: {
+      [P in Collection]?: {
+        [k: string]: {distinct_dids: number; records: number} | undefined
+      }
+    }
+  }
+  const links = json.links
+  return {
+    likeCount:
+      links?.['app.bsky.feed.like']?.['.subject.uri']?.distinct_dids ?? 0,
+    repostCount:
+      links?.['app.bsky.feed.repost']?.['.subject.uri']?.distinct_dids ?? 0,
+    replyCount:
+      links?.['app.bsky.feed.post']?.['.reply.parent.uri']?.records ?? 0,
+  }
+}
+
+export function asUri(link: ConstellationLink): string {
+  return `at://${link.did}/${link.collection}/${link.rkey}`
+}
+
+export async function* asyncGenMap<K, V>(
+  gen: AsyncGenerator<K, void, unknown>,
+  fn: (_: K) => V,
+) {
+  for await (const v of gen) {
+    yield fn(v)
+  }
+}
+
+export async function* asyncGenFilter<K>(
+  gen: AsyncGenerator<K, void, unknown>,
+  predicate: (_: K) => boolean,
+) {
+  for await (const v of gen) {
+    if (predicate(v)) yield v
+  }
+}
+
+export async function asyncGenCollect<V>(
+  gen: AsyncGenerator<V, void, unknown>,
+) {
+  const out = []
+  for await (const v of gen) {
+    out.push(v)
+  }
+  return out
+}
